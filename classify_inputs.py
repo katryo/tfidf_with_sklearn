05b1e60374c00ca3eebc58cff297e5ac31bf720f
katryo <katoryo55@gmail.com>
2013-12-15 18:34:51 +0900

    pdbとった

--- classify_inputs.py
+++ classify_inputs.py
@@ -1,11 +1,11 @@
+# -*- coding: utf-8 -*-
 import pickle
 import constants
 import sys
-import pdb
 
 if __name__ == '__main__':
     with open(constants.NB_PKL_FILENAME, 'rb') as f:
         nb = pickle.load(f)
     for query in sys.stdin:
         result = nb.classify(query)
-        print(result)
+        print('推測されたカテゴリーは %s です' % result)
